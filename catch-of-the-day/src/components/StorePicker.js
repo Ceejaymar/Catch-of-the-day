8d874231ce67ba25e43d2bdeaa8327040d3a3716
Carlos Martinez <martinezcj2@gmail.com>
2017-07-25 17:25:16 -0400

    Add goToStore function

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
-    render() {
-        return (
-            <form className="store-selector">
-                  <h2>Please choose a store</h2> 
-                 <input type="text" required placeholder='Store name' />
-                 <button>Visit store</button>
-            </form>
-        )
-    }
+  // constructor(){
+  //   super()
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+  goToStore(e){
+    e.preventDefault();
+
+    console.log(this.storeInput.value)
+  }
+
+  render() {
+    return (
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
+        <h2>Please choose a store</h2> 
+        <input type="text" required placeholder='Store name' defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
+        <button>Visit store</button>
+      </form>
+    )
+  }
 }
 
 export default StorePicker;
