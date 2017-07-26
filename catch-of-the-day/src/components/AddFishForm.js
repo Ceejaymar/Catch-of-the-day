1cd5fb67243b571b83c260ab35114348e7a9f114
Carlos Martinez <martinezcj2@gmail.com>
2017-07-25 21:45:23 -0400

    Create addFishForm

---
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class AddFishForm extends Component {
+  createFish(e){
+    e.preventDefault();
+    console.log('Gonna make some fish');
+
+    const fish = {
+      name: this.name.value,
+      price: this.price.value,
+      status: this.status.value,
+      desc: this.desc.desc,
+      image: this.image.value
+    }
+    this.props.addFish(fish);
+    this.fishForm.reset();
+  }
+
+  render() {
+    return (
+      <form ref={(input) => this.fishForm = input} className='fish-edit' onSubmit={(e) => (this.createFish(e))}>
+        <input ref={(input) => this.name = input} type="text" placeholder='fish Name'/>
+        <input ref={(input) => this.price = input} type="text" placeholder='fish Price'/>
+        <select ref={(input) => this.status = input}>
+          <option value="available">Fresh !</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textArea ref={(input) => this.desc = input} type="text" placeholder='fish Desc'/>
+        <input ref={(input) => this.image = input} type="text" placeholder='fish Image'/>
+        <button type='submit'>Submit</button>
+      </form>
+    )
+  }
+}
+
+export default AddFishForm;
