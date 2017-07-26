0a649c754bff880b7f02eea5cf57b5c3c46c9078
Carlos Martinez <martinezcj2@gmail.com>
2017-07-26 15:33:34 -0400

    Import firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAGYbzjII-Ml8cFauRze579z7nuJs-M_Kg",
+  authDomain: "catch-of-the-day-carlos-52f0d.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-carlos-52f0d.firebaseio.com"
+})
+
+export default base;
